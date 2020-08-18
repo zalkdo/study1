64a43af7ada86788a872550d7e2fcbeadead22d9
jaehuen.kim <jh_kim@emro.co.kr>
2020-08-18 18:05:14 +0900

    필요한 ip만 추출

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,19 +1,34 @@
 package com.example.demo;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
-import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
 	@GetMapping("/")
-	public String hello() throws UnknownHostException {
-		InetAddress local = InetAddress.getLoopbackAddress();
-		InetAddress host  = InetAddress.getLocalHost();
+	public String hello() throws UnknownHostException, SocketException {
+		String ips = "";
+		Enumeration e = NetworkInterface.getNetworkInterfaces();
+		while(e.hasMoreElements()){
+			NetworkInterface n = (NetworkInterface) e.nextElement();
+			Enumeration ee = n.getInetAddresses();
+			while(ee.hasMoreElements()){
+				InetAddress i = (InetAddress) ee.nextElement();
+				if(!(i.isLoopbackAddress()||i.isLinkLocalAddress())&&i.isSiteLocalAddress())
+					ips += "\t-"+n.getName() + " :: "+i.getHostAddress() + " \n";
+			}
+		}
+//		InetAddress local = InetAddress.getLoopbackAddress();
+//		InetAddress host  = InetAddress.getLocalHost();
 
-		return host.getHostName() + " :: " + host.getHostAddress() + " :: " + local.toString();
+//		return host.getHostName() + " :: " + host.getHostAddress() + " :: " + local.toString();
+		return ips;
 	}
 
 }
